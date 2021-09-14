7fb2229cd7da992641216737f0ca1df3efb55ee8
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-09-13 22:24:23 -0400

    now counting the rows for the number of samples

--- hw1.py
+++ hw1.py
@@ -3,13 +3,16 @@
 # Compares the training errors of various features on a given dataset.
 #
 # By        : Leomar Durán <https://github.com/lduran2/>
-# When      : 2021-09-13t21:53
+# When      : 2021-09-13t22:24
 # For       : CIS 4526
-# Version   : 1.1
+# Version   : 1.2
 #
 # Changelog :
-#     v1.0 - 2021-09-13t21:53
-#         reading the CSV files, got the names of the features
+#     v1.2 - 2021-09-13t22:24
+#         now counting the rows for the number of samples
+#
+#     v1.1 - 2021-09-13t21:53
+#         reading the names of the features from the CSV files
 #
 #     v1.0 - 2021-09-13t21:42
 #         added argument support, cleaned up the template a bit
@@ -21,6 +24,9 @@ import sys  # for argv
 import csv  # for DictReader
 
 def main():
+    r'''
+    Processes each CSV file passed as an argument.
+    '''
     # loop through the arguments (slice off argv[0], the callee)
     for arg in sys.argv[1:]:
         main_arg(arg)
@@ -28,18 +34,41 @@ def main():
 # end def main()
 
 def main_arg(arg):
-    # Read input file and save attribute names
+    r'''
+    Processes a single CSV file.
+    '''
+    # open the CSV file
     with open(arg) as csvfile:
+        # initialize number of examples at 0
+        num_examples = 0
+
+        # Read input file and save attribute names
+        # place a dictionary reader on it
         reader = csv.DictReader(csvfile)
+        # read in the first row of fields
         attr_names = reader.fieldnames
+        # get the number of features
         num_attr = len(attr_names)
 
-    # Compute accuracy for each feature
-    attr_train_err = [30.0223] * num_attr
+        # initialize favorable examples at 0
+        attr_train_favorable = [0] * num_attr
+
+        # find number of favorable outcomes
+        for row in reader:
+            # count this row
+            num_examples = (num_examples + 1)
+            # print(row)
+        # end for row in reader
+
+        # Compute accuracy for each feature
+        attr_train_err = [(fav/(num_examples + 1)) for fav in attr_train_favorable]
+    # end with open(arg) as csvfile
 
     # Print out the results
     for attr, err in zip(attr_names, attr_train_err):
         print("Attribute: {}, Training Error: {}".format(attr, round(err, 2)))
+    # end for attr, err in zip(attr_names, attr_train_err):
+# def main_arg(arg)
 
 if (__name__ == "__main__"):
     main()
