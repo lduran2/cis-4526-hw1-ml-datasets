ee63d4c9e80b3b337d848db6bcdbd6d203963c03
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-09-13 21:55:18 -0400

    reading the names of the features from the CSV files

--- hw1.py
+++ hw1.py
@@ -3,18 +3,22 @@
 # Compares the training errors of various features on a given dataset.
 #
 # By        : Leomar Durán <https://github.com/lduran2/>
-# When      : 2021-09-13t21:42
+# When      : 2021-09-13t21:53
 # For       : CIS 4526
-# Version   : 1.5
+# Version   : 1.1
 #
 # Changelog :
+#     v1.0 - 2021-09-13t21:53
+#         reading the CSV files, got the names of the features
+#
 #     v1.0 - 2021-09-13t21:42
 #         added argument support, cleaned up the template a bit
 #
 #     v0.0 - ????-??-??t??:??
 #         the tempate
 
-import sys
+import sys  # for argv
+import csv  # for DictReader
 
 def main():
     # loop through the arguments (slice off argv[0], the callee)
@@ -25,11 +29,13 @@ def main():
 
 def main_arg(arg):
     # Read input file and save attribute names
-    attr_names = ["apple", "banana", "carrot"]
-    num_attr = 3
+    with open(arg) as csvfile:
+        reader = csv.DictReader(csvfile)
+        attr_names = reader.fieldnames
+        num_attr = len(attr_names)
 
     # Compute accuracy for each feature
-    attr_train_err = [30.0223, 12.2, 1.1]
+    attr_train_err = [30.0223] * num_attr
 
     # Print out the results
     for attr, err in zip(attr_names, attr_train_err):
